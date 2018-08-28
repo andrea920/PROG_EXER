50368d9a3cb1467be54bc94a971e55547f62616f
andrea920 <41714257+andrea920@users.noreply.github.com>
2018-08-29 00:10:43 +0800

    Add files via upload

---
+++ PROG_EXER1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+    int f, e, a, b, c, d, x1, x2;
+    a=2;
+    b=4;
+    c=6;
+    f=4*a*c;
+    e=b*b;
+    d=sqrt((b*b)-(4*(a*c)));
+    x1=(-b+d)/(2*a);
+    x2=(-b-d)/(2*a);
+    cout<<"x1=" <<x1;
+    cout<<"\nx2=" <<x2;
+    return 0;
+}
